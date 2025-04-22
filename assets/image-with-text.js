c547c2b199199c16123c971f523980416be10f8e
Jacobo91 <jacobovelasquez1991@gmail.com>
2025-04-22 16:08:53 -0500

    fix js

--- assets/image-with-text.js
+++ assets/image-with-text.js
@@ -1,5 +1,6 @@
 const heading = document.querySelector('.image-with-text__heading.inline-richtext.h1.template--24453778112828__image_with_text_8Hiw8j');
 const heading2 = document.querySelector('.image-with-text__heading.inline-richtext.h1.template--24453778145596__image_with_text_tFmyhT');
+const heading3 = document.querySelector('.image-with-text__heading.inline-richtext.h1.template--24453778473276__image_with_text_AfhccY');
 
 if (heading) {
   const words = heading.textContent.trim().split(/\s+/); // Split on whitespace
@@ -25,4 +26,17 @@ if (heading2) {
   });
 
   heading2.innerHTML = limitedBreaks.join(' ');
+}
+
+if (heading3) {
+  const words = heading3.textContent.trim().split(/\s+/); // Split on whitespace
+  const limitedBreaks = words.map((word, index) => {
+    // Insert a <br> after every 2nd and 5th word (index 1 and 4)
+    if (index === 1 || index === 4) {
+      return word + '<br>';
+    }
+    return word;
+  });
+
+  heading3.innerHTML = limitedBreaks.join(' ');
 }
