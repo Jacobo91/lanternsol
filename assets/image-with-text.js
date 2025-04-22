883e55c8e342cc288bf323798418583ed3ba3b40
Jacobo91 <jacobovelasquez1991@gmail.com>
2025-04-22 12:08:05 -0500

    fix js

--- assets/image-with-text.js
+++ assets/image-with-text.js
@@ -1,4 +1,5 @@
-const heading = document.querySelector('.image-with-text__heading.inline-richtext.h1');
+const heading = document.querySelector('.image-with-text__heading.inline-richtext.h1.template--24453778112828__image_with_text_8Hiw8j');
+const heading2 = document.querySelector('.image-with-text__heading.inline-richtext.h1.template--24453778145596__image_with_text_tFmyhT');
 
 if (heading) {
   const words = heading.textContent.trim().split(/\s+/); // Split on whitespace
@@ -11,4 +12,17 @@ if (heading) {
   });
 
   heading.innerHTML = limitedBreaks.join(' ');
+}
+
+if (heading2) {
+  const words = heading2.textContent.trim().split(/\s+/); // Split on whitespace
+  const limitedBreaks = words.map((word, index) => {
+    // Insert a <br> after every 2nd and 5th word (index 1 and 4)
+    if (index === 1 || index === 4) {
+      return word + '<br>';
+    }
+    return word;
+  });
+
+  heading2.innerHTML = limitedBreaks.join(' ');
 }
